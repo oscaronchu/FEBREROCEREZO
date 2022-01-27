33fdc712b10be65106f273399afa15e51dcd2297
dam <dam@203P02>
2022-01-27 10:03:42 +0100

    Version 2

---
+++ src/cerezooscar/Nombre.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cerezooscar;
+
+/**
+ *
+ * @author dam
+ */
+public class Nombre {
+    public Nombre(){
+        System.out.println("SOY OSCAR");
+        System.out.println("Soy de burgos");
+    }
+}
